7edbe4a85f8c0df079fbeae5ba74880cea47d3f5
Katey Basye <kbasye@Kateys-MacBook-Pro.local>
2013-11-26 11:07:18 -0800

    Hide successful save indicator if program is already saved.

--- js/saved.js
+++ js/saved.js
@@ -3,6 +3,11 @@
 $('li.success').hide();
 
 $('li.bookmark').click(function(){
-	$(this).next().fadeIn(1000).fadeOut(1000);
+	if($(this).children('a').hasClass('success')) {
+		console.log('he');
+	}
+	else {
+		$(this).next().fadeIn(1000).fadeOut(1000);	
+	}
 	$(this).children('a').toggleClass('success')
 });
