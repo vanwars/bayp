4c6af0bcd25e0fb73df0c9f3c2890f8982afacfa
Katey Basye <kbasye@gmail.com>
2013-12-03 01:34:18 -0800

    Logged in logic for saving and recommending programs

--- js/saved.js
+++ js/saved.js
@@ -1,6 +1,4 @@
-// $('#myModal').modal(options);
-
-$('li.success').hide();
+$('.success').hide();
 
 $('li.bookmark').click(function(){
 	if($(this).children('a').hasClass('success')) {
@@ -11,3 +9,21 @@ $('li.bookmark').click(function(){
 	}
 	$(this).children('a').toggleClass('success')
 });
+
+$('a.recommend').click(function(){
+	var $rec_text = $(this).nextAll('small').children('span');
+	var $rec_num = parseInt($rec_text.text());
+	if($(this).children('span').hasClass('success')) {
+		$rec_num -= 1;
+		$rec_text.text($rec_num);
+		console.log('yes');
+	}
+	else {
+		$rec_num += 1;
+		$rec_text.text($rec_num);
+		$(this).nextAll('a.success').fadeIn(1000).fadeOut(1000);	
+	}
+	$(this).children('span').toggleClass('success');
+});
+
+$('#myModal').modal(options);
